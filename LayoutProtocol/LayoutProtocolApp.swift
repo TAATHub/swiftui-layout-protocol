9d4e682a2c2a71d6a236a46408cd174dfe1847ed
TAAT <20purpleblue10@gmail.com>
2023-01-17 12:40:31 +0900

    Initial Commit

---
+++ LayoutProtocol/LayoutProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutProtocolApp.swift
+//  LayoutProtocol
+//
+//  Created by 董 亜飛 on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct LayoutProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
