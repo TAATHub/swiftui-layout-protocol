eb85d094f95fca61de608eb6c99911b5038c18b5
TAAT <20purpleblue10@gmail.com>
2023-01-17 15:20:00 +0900

    [update] Add buttons to RadialLayoutView.

--- LayoutProtocol/View/RadialLayoutView.swift
+++ LayoutProtocol/View/RadialLayoutView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct RadialLayoutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Radial layout")
+        RadialLayout {
+            Text("A")
+                .frame(width: 40, height: 40)
+                .background(.red)
+                .clipShape(Circle())
+            Text("B")
+                .frame(width: 40, height: 40)
+                .background(.orange)
+                .clipShape(Circle())
+            Text("C")
+                .frame(width: 40, height: 40)
+                .background(.yellow)
+                .clipShape(Circle())
+            Text("D")
+                .frame(width: 40, height: 40)
+                .background(.green)
+                .clipShape(Circle())
+            Text("E")
+                .frame(width: 40, height: 40)
+                .background(.blue)
+                .clipShape(Circle())
+            Text("F")
+                .frame(width: 40, height: 40)
+                .background(.purple)
+                .clipShape(Circle())
+        }
+        .padding(40)
+        .navigationTitle("Radial layout")
     }
 }
 
